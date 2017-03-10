9f492f5578406f8522be725d4e53f5f5280a5c6e
鈴木慎太郎 <s.suzuki@wmshome.net>
2017-03-10 16:50:27 +0900

    first commit

---
+++ 040/devStuff/aigis_assets/js/main.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+  var toggle = document.getElementById('toggle');
+  var theme = document.getElementById('theme');
+  var sidemenuToggle = document.getElementById('sidemenu-toggle');
+  var codeToggle = document.getElementById('code-toggle');
+  var viewToggle = document.getElementById('view-toggle');
+  var previewWidth = document.getElementById("previewWidth");
+  var preview = document.getElementsByClassName("aigis-preview");
+
+  sidemenuToggle.onclick = function() {
+    toggle.classList.toggle('sidemenu-is-closed');
+  }
+  codeToggle.onclick = function() {
+    toggle.classList.toggle('code-is-closed');
+  }
+  viewToggle.onclick = function() {
+    if (theme.getAttribute('href').match(/pc/)) {
+      var view = theme.getAttribute('href').replace(/pc-L25/,'sp');
+      theme.href = view;
+    } else {
+      var view = theme.getAttribute('href').replace(/sp/,'pc-L25');
+      theme.href = view;
+    }
+    toggle.classList.toggle('view-is-sp');
+  }
+
+  function changeWidth() {
+    document.getElementById("showWidthArea").textContent = previewWidth.value;
+    for (var i=0;i<preview.length;i++) {
+      preview[i].style.width = previewWidth.value + 'px';
+    }
+  }
+  previewWidth.addEventListener("change", changeWidth, false);
+}
+
+
+
