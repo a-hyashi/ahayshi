04b41867170ac0544f880a839c94b916ef6697f8
鈴木慎太郎 <s.suzuki@wmshome.net>
2017-06-21 17:35:37 +0900

    スタイルガイドでパンくずリストを非表示にしないように暫定対応

--- common/devStuff/aigis_assets/js/sp.js
+++ common/devStuff/aigis_assets/js/sp.js
@@ -118,7 +118,7 @@ $(function(){
   var site_name_tag = getSiteName();
   var tel_tag = getTelNumber();
   var menu_tag = getMainMenuAccordion();
-  replaceTopicPath();
+  // replaceTopicPath();
   createHead(site_name_tag, tel_tag, menu_tag);
   createGnavAccordion();
   createSideMenuAccordion();
@@ -152,19 +152,19 @@ $(function(){
     }
   });
 
-  //トップページのみパンくず部品削除
-  var url_arr = location.href.split('/');
-  //公開ページ用
-  if(url_arr[3] === '' || url_arr[3] === 'index.html') {
-    if($('.mod-part-topicpath')[0]) $('.mod-part-topicpath').remove();
-  }
-  //CMS プレビュー用
-  if((url_arr[2] === 'www.next-cms-dev.com' ||
-      url_arr[2] === 'stg.next.blogdehp.jp' ||
-      url_arr[2] === 'www.wms-sample.com' ||
-      url_arr[2] === 'www.akibare-hp.com') && url_arr[3] === 'cms_v2') {
-    if($('.mod-part-topicpath')[0]) removePartTopicpathForCMS();
-  }
+  // //トップページのみパンくず部品削除
+  // var url_arr = location.href.split('/');
+  // //公開ページ用
+  // if(url_arr[3] === '' || url_arr[3] === 'index.html') {
+  //   if($('.mod-part-topicpath')[0]) $('.mod-part-topicpath').remove();
+  // }
+  // //CMS プレビュー用
+  // if((url_arr[2] === 'www.next-cms-dev.com' ||
+  //     url_arr[2] === 'stg.next.blogdehp.jp' ||
+  //     url_arr[2] === 'www.wms-sample.com' ||
+  //     url_arr[2] === 'www.akibare-hp.com') && url_arr[3] === 'cms_v2') {
+  //   if($('.mod-part-topicpath')[0]) removePartTopicpathForCMS();
+  // }
 
 });
 
@@ -399,24 +399,24 @@ function createSideMenuAccordion() {
   }
 }
 
-function replaceTopicPath() {
-  var topicpath = '';
-  var tag = '<div>';
-  var obj = '';
+// function replaceTopicPath() {
+//   var topicpath = '';
+//   var tag = '<div>';
+//   var obj = '';
 
-  if($('.mod-part-topicpath-header')[0]) {
-    obj = $('.mod-part-topicpath-header').first();
-    $('.mod-part-topicpath-header').remove();
-  }
-  if($('.mod-part-topicpath')[0]) {
-    obj = $('.mod-part-topicpath').first();
-    $('.mod-part-topicpath').remove();
-  }
-  if(obj != '') {
-    topicpath = $(tag).append(obj.clone()).html();
-    $('[class*="area-main-inner"]').last().append(topicpath);
-  }
-}
+//   if($('.mod-part-topicpath-header')[0]) {
+//     obj = $('.mod-part-topicpath-header').first();
+//     $('.mod-part-topicpath-header').remove();
+//   }
+//   if($('.mod-part-topicpath')[0]) {
+//     obj = $('.mod-part-topicpath').first();
+//     $('.mod-part-topicpath').remove();
+//   }
+//   if(obj != '') {
+//     topicpath = $(tag).append(obj.clone()).html();
+//     $('[class*="area-main-inner"]').last().append(topicpath);
+//   }
+// }
 
 
 function rearrangeParts(btype, obj, obj2) {
@@ -468,16 +468,16 @@ function rearrangeMod2col() {
   });
 }
 
-function removePartTopicpathForCMS(suffix) {
-  suffix = suffix || '';
-  var remove_flg = true
-  var part_elm = $('.mod-part-topicpath' + suffix);
-  part_elm.each(function() {
-    i = $(this).find('[itemscope]').length;
-    if(i > 1) remove_flg = false
-  });
-  if(remove_flg) part_elm.remove();
-}
+// function removePartTopicpathForCMS(suffix) {
+//   suffix = suffix || '';
+//   var remove_flg = true
+//   var part_elm = $('.mod-part-topicpath' + suffix);
+//   part_elm.each(function() {
+//     i = $(this).find('[itemscope]').length;
+//     if(i > 1) remove_flg = false
+//   });
+//   if(remove_flg) part_elm.remove();
+// }
 
 $(function() {
   $("[class^=b-sideMenu] label").on("click", function() {
