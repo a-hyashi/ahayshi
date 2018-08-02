de9c3897d81e879befdb18c53fe951008c78ab6b
蟹江 隼人 <>
2018-08-02 20:48:53 +0900

    面倒なのでコピーファイルを1つにする

--- copy.sh
+++ copy.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 # 実行ディレクトリが変わってしまうため、シンボリックリンクではなくコピーする
-rm -r ./gulpfile.js
-cp -r ../gulpfile.js ./gulpfile.js
+if [ -e ./gulpfile.js ]; then
+  rm -r ./gulpfile.js
+  echo "gulpfile.jsを削除しました"
+fi
+cp ../gulpfile.js ./gulpfile.js
 echo "gulpfile.jsのコピーが完了しました"
 
-rm -r ./lib/
+if [ -e ./lib/ ]; then
+  rm -r ./lib/
+  echo "libを削除しました"
+fi
 cp -r ../lib/ ./lib/
 echo "libのコピーが完了しました"
