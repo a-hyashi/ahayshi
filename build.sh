8c9fb07ea4a5cfe77804878d09e2d9edb8f64f20
蟹江 隼人 <>
2018-09-05 16:33:24 +0900

    テーマを指定しない場合は全テーマビルドする

--- build.sh
+++ build.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-themes=()
+# コードが汚いのでわかる方はリファクタしてください
 
 # 引数がある場合は引数のテーマを出力
 if [ $1 ] ; then
-  themes+=$1
+  sed -e 's/theme-name/'$1'/g' _docker-compose.yml > docker-compose.yml 
+  docker-compose run base gulp build
 # ない場合は全テーマを出力
 else
-  for filepath in `find . -type d -regex "./[0-9]{3}"` ; do
-    echo $filepath
+  for theme in `find . -type d -regex "./*[0-9][0-9][0-9]"` ; do
+    sed -e 's/theme-name/'${theme##*/}'/g' _docker-compose.yml > docker-compose.yml 
+    docker-compose run base gulp build
+  done
+  for theme in `find . -type d -regex "./*[0-9][0-9][0-9][A-Z]"` ; do
+    sed -e 's/theme-name/'${theme##*/}'/g' _docker-compose.yml > docker-compose.yml 
+    docker-compose run base gulp build
   done
 fi
-
-for theme in $themes
-do
-echo $theme
-:
-#  sed -e 's/theme-name/'$theme'/g' _docker-compose.yml > docker-compose.yml 
-#  docker-compose run base gulp build
-done
