7959dcac7b82e196c82f152696ea02d2d4ada8e2
蟹江 隼人 <>
2018-10-23 16:33:38 +0900

    parts/allに追記

---
+++ 031/add_all.rb
@@ -0,0 +1,12 @@
+PARTS = <<-PARTS
+@import '1001_frame1colD';
+@import '1002_frame2colD';
+@import '1003_frame3colD';
+@import '1004_frame4col';
+@import '1005_frame5col';
+@import '1006_frame6col';
+@import '1007_frame7col';
+PARTS
+
+old_file = File.open("./devStuff/src/parts/_all.scss").read
+File.open("./devStuff/src/parts/_all.scss", "w").puts("#{old_file.chomp.chomp}\n#{PARTS}")
