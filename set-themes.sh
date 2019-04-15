7b6c2c986ecf72aa219db63ddf16971b74ae04b4
kanie <kanie@akibare.net>
2019-04-15 20:51:55 +0900

    表記をコンテナに変更

--- set-themes.sh
+++ set-themes.sh
@@ -5,6 +5,6 @@ THEMES=($@)
 for i in "${!THEMES[@]}"; do
   num=$(($i+1))
   sed -e "s/#{theme-name}/${THEMES[i]}/g" -e "s/#{port-num}/300${num}/g"  -e "s/#{num}/${num}/g" _docker-compose.yml >> docker-compose.yml
-  printf "テーマ${num}：${THEMES[i]}\n"
+  printf "コンテナ${num}：${THEMES[i]}\n"
 done
 printf "\e[36mテーマを設定しました\e[m\n"
