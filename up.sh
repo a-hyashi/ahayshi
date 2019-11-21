8589df3285f7c5b6cea03bc2d9a67f7a5fd68e5c
Kazuma Otashiro <ootashiro@wmshome.net>
2019-11-21 15:18:54 +0900

    エラーに気づかないのでフォアグラウンド実行に戻した

--- up.sh
+++ up.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-printf "[Info] スタイルガイドをバックグランドで起動します...\n"
+#引数がある場合は指定のテーマで実行
+#ない場合は現在のテーマで実行
+if [ $1 ] ; then
+  for theme in "$@" ; do
+    ./set-themes.sh ${theme##*/}
+  done
+fi
+
+printf "\e[36m[Info] スタイルガイドを起動します...\e[m\n"
 printf "\e[36m001~099 URL: http://localhost:3001\e[m\n"
 printf "\e[36m100~    URL: http://localhost:3002\e[m\n\n"
 
-docker-compose up -d
+docker-compose up
