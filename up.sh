f69f5cbbc8baf5d1eab80843320f7a28b2daf14d
Kazuma Otashiro <ootashiro@wmshome.net>
2019-11-21 15:48:17 +0900

    改行

--- up.sh
+++ up.sh
@@ -8,7 +8,7 @@ if [ $1 ] ; then
   done
 fi
 
-printf "\e[36m[Info] スタイルガイドを起動します...\e[m\n"
+printf "\e[36m[Info] スタイルガイドを起動します...\e[m\n\n"
 printf "\e[36m001~099 URL: http://localhost:3001\e[m\n"
 printf "\e[36m100~    URL: http://localhost:3002\e[m\n\n"
 
