e092e4b5952df9d93daee5af2118a9092473a1de
Kazuma Otashiro <ootashiro@wmshome.net>
2019-11-25 16:06:21 +0900

    同時に複数のテーマで起動できるように

--- set-themes.sh
+++ set-themes.sh
@@ -4,8 +4,15 @@ rm docker-compose.yml
 echo version: \"3\" >> docker-compose.yml
 echo services: >> docker-compose.yml
 THEMES=($@)
-for i in "${!THEMES[@]}"; do
-  num=$(($i+1))
-  sed -e "s/#{theme-name}/${THEMES[i]}/g" -e "s/#{port-num}/300${num}/g"  -e "s/#{num}/${num}/g" _docker-compose.yml >> docker-compose.yml
-  printf "\e[36m[Info] テーマを${THEMES[i]}に設定しました\e[m\n"
+num=0
+for i in ${!THEMES[@]}; do
+  num=$(($num+1))
+  num2=$(($num+1))
+  sed -e "s/#{theme-name}/${THEMES[i]}/g" -e "s/#{num}/${num}/g" -e "s/#{num2}/${num2}/g" _docker-compose.yml >> docker-compose.yml
+  if [ $# -ge 2 ]; then
+    printf "\e[36m[Info] $(($i+1))つめのテーマを${THEMES[i]}に設定しました\e[m\n"
+  else
+    printf "\e[36m[Info] テーマを${THEMES[i]}に設定しました\e[m\n"
+  fi
+  num=$(($num+1))
 done
--- up.sh
+++ up.sh
@@ -2,14 +2,24 @@
 
 #引数がある場合は指定のテーマで実行
 #ない場合は現在のテーマで実行
+THEMES=($@)
 if [ $1 ] ; then
-  for theme in "$@" ; do
-    ./set-themes.sh ${theme##*/}
+  ./set-themes.sh ${THEMES[@]}
+    printf "\e[36m\n[Info] 複数のスタイルガイドをバックグランドで起動します...\e[m\n"
+  for i in ${!THEMES[@]}; do
+    num=$(($num+1))
+    num2=$(($num+1))
+    printf "\e[36m[Info] ${THEMES[i]} block 001~099 - URL: http://localhost:300${num}\e[m\n"
+    printf "\e[36m       ${THEMES[i]} block 100~    - URL: http://localhost:300${num2}\e[m\n"
+    if [ $# -ge 2 ]; then
+      docker-compose up -d bt${num}
+      docker-compose up -d bt${num2}
+      num=$(($num+1))
+    fi
   done
+else
+  printf "\e[36m[Info] スタイルガイドを起動します...\e[m\n\n"
+  printf "\e[36m[Info] 001~099 - URL: http://localhost:300${num}\e[m\n"
+  printf "\e[36m       100~    - URL: http://localhost:300${num2}\e[m\n"
+  docker-compose up
 fi
-
-printf "\e[36m[Info] スタイルガイドを起動します...\e[m\n\n"
-printf "\e[36m001~099 URL: http://localhost:3001\e[m\n"
-printf "\e[36m100~    URL: http://localhost:3002\e[m\n\n"
-
-docker-compose up
