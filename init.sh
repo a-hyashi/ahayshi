5fc3da3f5a72d3441a0205baa3a9349270b85616
Kazuma Otashiro <ootashiro@wmshome.net>
2019-11-27 10:33:11 +0900

    コンテナ名をappに

--- init.sh
+++ init.sh
@@ -9,23 +9,23 @@ if [ $1 ] ; then
     printf "\e[36m[Info] 全テーマでinitを実行します\e[m\n"
     for theme in `find . -type d -regex "./*[0-9][0-9][0-9][A-Z]*"` ; do
       ./set-themes.sh ${theme##*/}
-      docker-compose run bt1 cp ../copy.sh ./copy.sh
-      docker-compose run bt1 ./copy.sh
-      docker-compose run bt1 npx gulp update-styleguide
+      docker-compose run app1 cp ../copy.sh ./copy.sh
+      docker-compose run app1 ./copy.sh
+      docker-compose run app1 npx gulp update-styleguide
     done
   else
     for theme in "$@" ; do
       printf "\e[36m[Info] ${theme##*/}でinitを実行します\e[m\n"
       ./set-themes.sh ${theme##*/}
-      docker-compose run bt1 cp ../copy.sh ./copy.sh
-      docker-compose run bt1 ./copy.sh
-      docker-compose run bt1 npx gulp update-styleguide
+      docker-compose run app1 cp ../copy.sh ./copy.sh
+      docker-compose run app1 ./copy.sh
+      docker-compose run app1 npx gulp update-styleguide
     done
   fi
 else
-  docker-compose run bt1 cp ../copy.sh ./copy.sh
-  docker-compose run bt1 ./copy.sh
-  docker-compose run bt1 npx gulp update-styleguide
+  docker-compose run app1 cp ../copy.sh ./copy.sh
+  docker-compose run app1 ./copy.sh
+  docker-compose run app1 npx gulp update-styleguide
 fi
 
 docker-compose down
