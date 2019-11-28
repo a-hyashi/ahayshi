9804a8652a7c13e54f7b7f776fead1b03b2c6999
Kazuma Otashiro <ootashiro@wmshome.net>
2019-11-28 10:14:03 +0900

    messageの出し方を変更

--- up.sh
+++ up.sh
@@ -5,16 +5,28 @@
 THEMES=($@)
 if [ $1 ] ; then
   ./set-themes.sh ${THEMES[@]}
-  if [[ `ifconfig | grep 192.168.0.` =~ 192.168.0.[0-9]{1,3} ]]; then
-    printf "\n\e[32m[Info] 外部URL: \e[34mhttp://"${BASH_REMATCH[0]}"\e[m\n"
-  fi
+  printf "\n\e[36m[Info] スタイルガイドを起動します...\e[m\n\n"
+  printf "\e[32m[Info] Access URLs:\e[m\n"
   for i in ${!THEMES[@]}; do
     num1=$(($i*2+1))
     num2=$(($i*2+2))
-    printf "\e[32m${THEMES[i]} block 001~099 - URL: \e[34mhttp://localhost:300${num1}\e[m\n"
-    printf "\e[32m${THEMES[i]} block 100~    - URL: \e[34mhttp://localhost:300${num2}\e[m\n"
+    printf "\e[36mテーマ${THEMES[i]}\e[m\n"
+    printf "\e[32mlocal 001~099    URL: \e[34mhttp://localhost:300${num1}\e[m\n"
+    printf "\e[32mlocal 100~       URL: \e[34mhttp://localhost:300${num2}\e[m\n"
+    if [[ `ifconfig | grep 192.168.0.` =~ 192.168.0.[0-9]{1,3} ]]; then
+      printf "\e[32mExternal 001~099 URL: \e[34mhttp://"${BASH_REMATCH[0]}":300${num1}\e[m\n"
+      printf "\e[32mExternal 100~    URL: \e[34mhttp://"${BASH_REMATCH[0]}":300${num2}\e[m\n"
+    fi
+    echo "-----------------------------------------------------"
   done
+else
+  printf "\n\e[32m[Info] Access URLs:\e[m\n"
+  printf "\e[32mlocal 001~099    URL: \e[34mhttp://localhost:300${num1}\e[m\n"
+  printf "\e[32mlocal 100~       URL: \e[34mhttp://localhost:300${num2}\e[m\n"
+  if [[ `ifconfig | grep 192.168.0.` =~ 192.168.0.[0-9]{1,3} ]]; then
+    printf "\e[32mExternal 001~099 URL: \e[34mhttp://"${BASH_REMATCH[0]}":3002\e[m\n"
+    printf "\e[32mExternal 100~    URL: \e[34mhttp://"${BASH_REMATCH[0]}":3002\e[m\n"
+  fi
 fi
 
-printf "\n\e[36m[Info] スタイルガイドを起動します...\e[m\n\n"
 docker-compose up
