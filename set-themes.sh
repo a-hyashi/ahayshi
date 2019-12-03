5cbfff72a6e9e8467ede2d0c5c2fbc67f73de8f1
Kazuma Otashiro <ootashiro@wmshome.net>
2019-12-03 18:00:36 +0900

    set-themesは引数なしならすぐ終了させる

--- set-themes.sh
+++ set-themes.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-rm docker-compose.yml
-echo version: \"3\" >> docker-compose.yml
-echo services: >> docker-compose.yml
 if [ "$1" = "" ] ; then
   printf "\e[31m[Error] テーマを指定してください\e[m\n"
   exit 1
 fi
 
 THEMES=($@)
+rm docker-compose.yml
+echo version: \"3\" >> docker-compose.yml
+echo services: >> docker-compose.yml
+
 for i in ${!THEMES[@]}; do
   container_num=$(($i+1))
   sed -e "s/#{theme-name}/${THEMES[i]}/g" -e "s/#{num}/${container_num}/g" _docker-compose.yml >> docker-compose.yml
-  printf "\e[32m[Info] app${container_num}をテーマ${THEMES[i]}に設定しました\e[m\n"
+  printf "\e[32mapp${container_num}をテーマ${THEMES[i]}に設定しました\e[m\n"
 done
