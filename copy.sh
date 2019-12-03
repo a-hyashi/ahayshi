f91a0d2d6c874973340f1f234492fc7073a71992
Kazuma Otashiro <ootashiro@wmshome.net>
2019-12-03 18:22:08 +0900

    copy.shは全テーマにコピーするように

--- copy.sh
+++ copy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# 実行ディレクトリが変わってしまうため、シンボリックリンクではなくコピーする
-\cp ../gulpfile.js ./
-printf "\e[32m[Info] gulpfile.jsをコピーしました\e[m\n"
+# gulpの実行ディレクトリが変わってしまうため、シンボリックリンクではなくコピーする
+THEMES=(`find . -type d -regex "./[0-9][0-9][0-9][A-Z]*" | sed -e "s/\.\///"`)
+for i in ${!THEMES[@]}; do
+  \cp ./gulpfile.js ./${THEMES[i]}/
+  if [ -e ./${THEMES[i]}/lib/ ]; then
+    rm -r ./${THEMES[i]}/lib/
+  fi
+  \cp -r ./lib/ ./${THEMES[i]}/lib/
+done
 
-if [ -e ./lib/ ]; then
-  rm -r ./lib/
-fi
-\cp -r ../lib/ ./
-printf "\e[32m[Info] lib/をコピーしました\e[m\n"
+printf "\e[32mgulpfile.jsを各テーマフォルダにコピーしました\e[m\n"
+printf "\e[32mlib/を各テーマフォルダにコピーしました\e[m\n"
