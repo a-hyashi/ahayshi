c07453f3cd4079d6ac0201e14caa6fd0ef982c1d
Kazuma Otashiro <ootashiro@wmshome.net>
2019-12-06 12:13:21 +0900

    ログを1行にまとめた

--- copy.sh
+++ copy.sh
@@ -10,5 +10,4 @@ for i in ${!THEMES[@]}; do
   \cp -r ./lib/ ./${THEMES[i]}/lib/
 done
 
-printf "\e[32mgulpfile.jsを各テーマフォルダにコピーしました\e[m\n"
-printf "\e[32mlib/を各テーマフォルダにコピーしました\e[m\n"
+printf "\e[32mgulpfile.jsとlibのコピーが完了しました\e[m\n"
