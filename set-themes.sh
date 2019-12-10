598d897534b5a011fc4daf5ff42936439df4223f
kanie <kanie@akibare.net>
2019-12-10 15:02:12 +0900

    変数を直前で宣言する

--- set-themes.sh
+++ set-themes.sh
@@ -5,11 +5,11 @@ if [ "$1" = "" ] ; then
   exit 1
 fi
 
-THEMES=($@)
 rm docker-compose.yml
 echo version: \"3\" >> docker-compose.yml
 echo services: >> docker-compose.yml
 
+THEMES=($@)
 for i in ${!THEMES[@]}; do
   container_num=$(($i+1))
   sed -e "s/#{theme-name}/${THEMES[i]}/g" -e "s/#{num}/${container_num}/g" _docker-compose.yml >> docker-compose.yml
