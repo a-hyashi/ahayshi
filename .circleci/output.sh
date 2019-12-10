eed98c519f7b6804afd709c787b0c7ce685be128
kanie <kanie@akibare.net>
2019-12-10 20:35:57 +0900

    buildは毎回チェック、outputはmaster更新時のみ実行

--- .circleci/output.sh
+++ .circleci/output.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git clone git@github.com:wmssystem/ACRE-theme.git ../ACRE-theme
+
 # bash vertion3では連想配列が使えないため同じindexの値を取得して使う
 pips=()
 themes=()
@@ -10,7 +12,7 @@ count=0
 for theme in `find . -type d -regex "./*[0-9][0-9][0-9][A-Z]*"` ; do
   ./copy.sh ${theme##*/}
   cd ${theme##*/}
-  gulp build &
+  (gulp build && gulp output) &
   pips+=($!)
   themes+=( ${theme##*/} )
   cd ../
@@ -39,7 +41,18 @@ for ((i = 0; i < ${#pips[@]}; i++)) ; do
 done
 
 if [ ${#error_themes[@]} -ne 0 ] ; then
-  echo "以下のテーマのビルドに失敗しました"
+  echo "以下のテーマの出力に失敗しました"
   echo "${error_themes[@]}"
   exit 1
 fi
+
+cd ../ACRE-theme
+
+d=`date '+%Y%m%d%H%M%S'`
+
+git config --global user.email "system@akibare.net"
+git config --global user.name "syanai-deploy"
+git checkout -b "circleCI/${d}"
+git add .
+git commit -m "buddy-themeから出力"
+git push origin "circleCI/${d}"
