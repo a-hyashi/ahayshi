248de831ea04f8f233627417f5e80769c6035605
kanie <kanie@akibare.net>
2020-02-12 14:31:11 +0900

    並列件数を減らす

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -18,7 +18,7 @@ for theme in `find . -type d -regex "./*[0-9][0-9][0-9][A-Z]*"` ; do
   count=$(( count + 1 ))
   # 一括処理だとメモリが不足して以下のエラーが発生するため分割して実行
   # ./.circleci/build.sh: line 9:   999 Killed
-  if [ ${count} -eq 12 ] ; then
+  if [ ${count} -eq 8 ] ; then
     for ((i = 0; i < ${#pips[@]}; i++)) ; do
       wait ${pips[$i]}
       if [ $? -ne 0 ]; then
