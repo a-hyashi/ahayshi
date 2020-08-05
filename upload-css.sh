963897f0208ac2eb8ae72d41200140ca88a3a2d1
kanie <kanie@akibare.net>
2020-08-05 16:27:11 +0900

    handshakeエラーを解消

--- upload-css.sh
+++ upload-css.sh
@@ -11,8 +11,6 @@ if [ $1 ] ; then
     for theme in `find . -type d -maxdepth 1 -regex "./[0-9][0-9][0-9][A-Z]*" | sort` ; do
       ./set-themes.sh ${theme#./}
       docker-compose run app1 npx gulp upload-css
-      docker-compose run app1 npx gulp upload-css-2
-      docker-compose run app1 npx gulp upload-css-3
     done
     docker-compose down
     printf "\e[32mアップロードが完了しました\e[m\n"
@@ -26,8 +24,6 @@ fi
 APPS=($(grep 'app[0-9]*' docker-compose.yml --only-matching))
 for ((i = 0; i < ${#APPS[@]}; i++)) ; do
   docker-compose run app$(($i+1)) npx gulp upload-css
-  docker-compose run app$(($i+1)) npx gulp upload-css-2
-  docker-compose run app$(($i+1)) npx gulp upload-css-3
 done
 
 docker-compose down
