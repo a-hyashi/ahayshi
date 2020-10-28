c295798a7fe78fdbb407d6186ecf74bc985b44dc
kanie <kanie@akibare.net>
2020-10-28 12:45:03 +0900

    リファクタ

--- gulpfile.js
+++ gulpfile.js
@@ -186,32 +186,70 @@ const to_td_line = (arr) => {
   return '|' + arr.join('|') + '|';
 }
 
-const create_b_placer = (b_placer_base, variation_match) => {
+const create_b_placer = (b_placer_base, matched_variation) => {
   // カテゴリー、エリア、名前は前にコメントで出てきた値を使う
   var b_placer = Object.assign(Object.create(Object.getPrototypeOf(b_placer_base)), b_placer_base);
-  b_placer.class_name = variation_match[1].trim();
-  if (variation_match[3]) b_placer.variation = variation_match[3].trim();
-  if (variation_match[5]) b_placer.pc_n00_value = variation_match[5].trim();
-  b_placer.pc_value = variation_match[6].trim();
+  b_placer.class_name = matched_variation[1].trim();
+  if (matched_variation[3]) b_placer.variation = matched_variation[3].trim();
+  if (matched_variation[5]) b_placer.pc_n00_value = matched_variation[5].trim();
+  b_placer.pc_value = matched_variation[6].trim();
   return b_placer;
 }
 
-const update_sp_value = (b_placers, variation_match) => {
+const update_sp_value = (b_placers, matched_variation) => {
   // PCで作成したb_placerを探し、そのレコードにSPの値を設定する
   var b_placer = b_placers.find((b) => {
-    variation = variation_match[3] ? variation_match[3].trim() : undefined;
-    return b.class_name === variation_match[1].trim() && b.variation === variation;
+    variation = matched_variation[3] ? matched_variation[3].trim() : undefined;
+    return b.class_name === matched_variation[1].trim() && b.variation === variation;
   });
-  b_placer.sp_value = variation_match[6].trim();
+  b_placer.sp_value = matched_variation[6].trim();
 }
 
-const output_b_placer_doc = (b_placers) => {
-  var th = ['カテゴリ', 'エリア', '部品名・要素名', 'クラス名', 'バリエーション', 'PC', 'PC<br>(N00)', 'SP'];
-  var table = [to_td_line(th), to_td_line('-'.repeat(th.length).split(''))];
+const to_md_text = (b_placers) => {
+  const th = ['カテゴリ', 'エリア', '部品名・要素名', 'クラス名', 'バリエーション', 'PC', 'PC<br>(N00)', 'SP'];
+  const th_separator_line = new Array(th.length).fill('-');
+  var md_table = [to_td_line(th), to_td_line(th_separator_line)];
   b_placers.forEach((b_placer) => {
-    table.push(b_placer.to_td_line());
+    md_table.push(b_placer.to_td_line());
   });
-  fs.writeFileSync('devStuff/docs/bPlacer.md', (table.join('\n')));
+  return md_table.join('\n');
+}
+
+const create_b_placers = () => {
+  // 一度出てきた情報を保持しておくために使います
+  // （例）一度01.見出しと出てくれば、次のが出てくるまでずっと01.見出し
+  var b_placer_base = new BPlacerRecord();
+  var b_placers = [];
+  var is_sp = false;
+  b_placer_lines().forEach(function(b_placer_line) {
+    // $device == "SP" 以降はSPの余白として設定する
+    has_matched_sp = b_placer_line.match(/\$device\s*==\s*[\'\"]SP[\'\"]/);
+    if (has_matched_sp) is_sp = true;
+    // //# で始まるコメントはカテゴリー
+    var matched_category = b_placer_line.match(/\/\#\s+(.*)/);
+    if (matched_category) b_placer_base.category = matched_category[1].trim();
+    // //## で始まるコメントはエリアと名前 | でエリアと名前を区切る
+    var matched_area_and_name = b_placer_line.match(/\/\##\s+(.*)/);
+    if (matched_area_and_name) {
+      var area_and_name = matched_area_and_name[1].trim().split('|');
+      b_placer_base.area = area_and_name[0].trim();
+      b_placer_base.name = area_and_name[1].trim();
+    }
+    // .t0-b-で始まる文字はバリエーション
+    // [a-zA-Z]がないと数字がバリエーション名の中に紛れてしまう
+    // 正規表現がややこしくなるため、スペース等はあまり考慮していません
+    // .t0-b-xxxxx(数字 or #{$...} or 無)-bPlacer{@if $layout == "N00" {padding-bottom:00;}@else{padding-bottom:99;}}
+    // [0]:全体, [1]:クラス名, [2]:t0-, [3]:バリエーション名, [4]:{}の中身（使わない）, [5]:N00がある場合の余白の値, [6]:N00でない余白の値
+    var matched_variation = b_placer_line.match(/(\.(t0-)?b-[\.\_\-a-zA-Z0-9]*[a-zA-Z])(\d*|\#\{\$[a-zA-Z0-9]+\})?-bPlacer{(.+N00.+padding-bottom:(.+?);)?.*padding-bottom:(.+?);}/);
+    if (matched_variation) {
+      if (!is_sp) {
+        b_placers.push(create_b_placer(b_placer_base, matched_variation));
+      } else {
+        update_sp_value(b_placers, matched_variation);
+      }
+    }
+  });
+  return b_placers;
 }
 
 gulp.task('create-b-placer-doc', (done) => {
@@ -221,40 +259,8 @@ gulp.task('create-b-placer-doc', (done) => {
     return;
   }
 
-  // 一度出てきた情報を保持しておくために使います
-  // （例）一度01.見出しと出てくれば、次のが出てくるまでずっと01.見出し
-  var b_placer_base = new BPlacerRecord();
-  var b_placers = [];
-  var is_sp = false;
-  b_placer_lines().forEach(function(line) {
-    // $device == "SP" 以降はSPの余白として設定する
-    sp_match = line.match(/\$device\s*==\s*[\'\"]SP[\'\"]/);
-    if (sp_match) is_sp = true;
-    // //# で始まるコメントはカテゴリー
-    var category_match = line.match(/\/\#\s+(.*)/);
-    if (category_match) b_placer_base.category = category_match[1].trim();
-    // //## で始まるコメントはエリアと名前 | でエリアと名前を区切る
-    var area_and_name_match = line.match(/\/\##\s+(.*)/);
-    if (area_and_name_match) {
-      var area_and_name = area_and_name_match[1].trim().split('|');
-      b_placer_base.area = area_and_name[0].trim();
-      b_placer_base.name = area_and_name[1].trim();
-    }
-    // .t0-b-で始まる文字はバリエーション
-    // [a-zA-Z]がないと数字がバリエーション名の中に紛れてしまう
-    // 正規表現がややこしくなるため、スペース等はあまり考慮していません
-    // .t0-b-xxxxx(数字 or #{$...} or 無)-bPlacer{@if $layout == "N00" {padding-bottom:00;}@else{padding-bottom:99;}}
-    // [0]:全体, [1]:クラス名, [2]:t0-, [3]:バリエーション名, [4]:{}の中身（使わない）, [5]:N00がある場合の余白の値, [6]:N00でない余白の値
-    var variation_match = line.match(/(\.(t0-)?b-[\.\_\-a-zA-Z0-9]*[a-zA-Z])(\d*|\#\{\$[a-zA-Z0-9]+\})?-bPlacer{(.+N00.+padding-bottom:(.+?);)?.*padding-bottom:(.+?);}/);
-    if (variation_match) {
-      if (!is_sp) {
-        b_placers.push(create_b_placer(b_placer_base, variation_match));
-      } else {
-        update_sp_value(b_placers, variation_match);
-      }
-    }
-  })
-  output_b_placer_doc(b_placers)
+  const b_placers = create_b_placers();
+  fs.writeFileSync('devStuff/docs/bPlacer.md', to_md_text(b_placers));
   done();
   }
 );
