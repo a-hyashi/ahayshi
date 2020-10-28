86c637c14f0532bc615aba612fce6fa33370f96e
kanie <kanie@akibare.net>
2020-10-28 15:00:31 +0900

    リファクタ

--- gulpfile.js
+++ gulpfile.js
@@ -152,7 +152,7 @@ gulp.task('update-sassdoc', () => {
   .pipe(sassdoc(options));
 });
 
-const b_placer_lines = () => {
+const b_placer_scss_lines = () => {
   var b_placer_scss = fs.readFileSync('devStuff/src/config/_bPlacer.scss', 'utf8');
   return b_placer_scss.toString().split('\n');
 }
@@ -168,6 +168,7 @@ class BPlacerRecord {
     this.pc_n00_value = pc_n00_value;
     this.sp_value = sp_value;
   }
+
   to_td_line() {
     return to_td_line([
       this.category,
@@ -183,73 +184,103 @@ class BPlacerRecord {
 }
 
 const to_td_line = (arr) => {
-  return '|' + arr.join('|') + '|';
+  return `|${arr.join('|')}|`;
 }
 
-const create_b_placer = (b_placer_base, matched_variation) => {
+class BPlacerline {
+  constructor(matched_line) {
+    this.matched_line = matched_line;
+  }
+
+  has_matched() {
+    return !!this.matched_line;
+  }
+
+  trim_index_of(index) {
+    return this.matched_line[index] ? this.matched_line[index].trim() : '';
+  }
+
+  class_name() {
+    return this.trim_index_of(1);
+  }
+
+  variation() {
+    return this.trim_index_of(3);
+  }
+
+  pc_n00_value() {
+    return this.trim_index_of(5);
+  }
+
+  value() {
+    return this.trim_index_of(6);
+  }
+}
+
+const create_b_placer_record = (b_placer_base_record, b_placer_line) => {
   // カテゴリー、エリア、名前は前にコメントで出てきた値を使う
-  var b_placer = Object.assign(Object.create(Object.getPrototypeOf(b_placer_base)), b_placer_base);
-  b_placer.class_name = matched_variation[1].trim();
-  if (matched_variation[3]) b_placer.variation = matched_variation[3].trim();
-  if (matched_variation[5]) b_placer.pc_n00_value = matched_variation[5].trim();
-  b_placer.pc_value = matched_variation[6].trim();
-  return b_placer;
+  var b_placer_record = Object.assign(Object.create(Object.getPrototypeOf(b_placer_base_record)), b_placer_base_record);
+  b_placer_record.class_name = b_placer_line.class_name();
+  b_placer_record.variation = b_placer_line.variation();
+  b_placer_record.pc_n00_value = b_placer_line.pc_n00_value();
+  b_placer_record.pc_value = b_placer_line.value();
+  return b_placer_record;
 }
 
-const update_sp_value = (b_placers, matched_variation) => {
+const set_sp_value = (b_placer_record_list, b_placer_line) => {
   // PCで作成したb_placerを探し、そのレコードにSPの値を設定する
-  var b_placer = b_placers.find((b) => {
-    variation = matched_variation[3] ? matched_variation[3].trim() : undefined;
-    return b.class_name === matched_variation[1].trim() && b.variation === variation;
+  var found_b_placer_record = b_placer_record_list.find((b_placer_record) => {
+    return b_placer_record.class_name === b_placer_line.class_name() && b_placer_record.variation === b_placer_line.variation();
   });
-  b_placer.sp_value = matched_variation[6].trim();
+  found_b_placer_record.sp_value = b_placer_line.value();
 }
 
-const to_md_text = (b_placers) => {
+const to_md_text = (b_placer_record_list) => {
   const th = ['カテゴリ', 'エリア', '部品名・要素名', 'クラス名', 'バリエーション', 'PC', 'PC<br>(N00)', 'SP'];
   const th_separator_line = new Array(th.length).fill('-');
   var md_table = [to_td_line(th), to_td_line(th_separator_line)];
-  b_placers.forEach((b_placer) => {
-    md_table.push(b_placer.to_td_line());
+  b_placer_record_list.forEach((b_placer_record) => {
+    md_table.push(b_placer_record.to_td_line());
   });
   return md_table.join('\n');
 }
 
-const create_b_placers = () => {
+const create_b_placer_record_list = () => {
   // 一度出てきた情報を保持しておくために使います
   // （例）一度01.見出しと出てくれば、次のが出てくるまでずっと01.見出し
-  var b_placer_base = new BPlacerRecord();
-  var b_placers = [];
+  var b_placer_base_record = new BPlacerRecord();
+  var b_placer_record_list = [];
   var is_sp = false;
-  b_placer_lines().forEach(function(b_placer_line) {
+  b_placer_scss_lines().forEach(function(b_placer_scss_line) {
     // $device == "SP" 以降はSPの余白として設定する
-    has_matched_sp = b_placer_line.match(/\$device\s*==\s*[\'\"]SP[\'\"]/);
-    if (has_matched_sp) is_sp = true;
+    matched_sp_line = b_placer_scss_line.match(/\$device\s*==\s*[\'\"]SP[\'\"]/);
+    if (matched_sp_line) is_sp = true;
     // //# で始まるコメントはカテゴリー
-    var matched_category = b_placer_line.match(/\/\#\s+(.*)/);
-    if (matched_category) b_placer_base.category = matched_category[1].trim();
+    const matched_category_line = b_placer_scss_line.match(/\/\#\s+(.*)/);
+    if (matched_category_line) b_placer_base_record.category = matched_category_line[1].trim();
     // //## で始まるコメントはエリアと名前 | でエリアと名前を区切る
-    var matched_area_and_name = b_placer_line.match(/\/\##\s+(.*)/);
-    if (matched_area_and_name) {
-      var area_and_name = matched_area_and_name[1].trim().split('|');
-      b_placer_base.area = area_and_name[0].trim();
-      b_placer_base.name = area_and_name[1].trim();
+    const matched_area_and_name_line = b_placer_scss_line.match(/\/\##\s+(.*)/);
+    if (matched_area_and_name_line) {
+      const area_and_name = matched_area_and_name_line[1].trim().split('|');
+      b_placer_base_record.area = area_and_name[0].trim();
+      b_placer_base_record.name = area_and_name[1].trim();
     }
-    // .t0-b-で始まる文字はバリエーション
+    // .t0-b-で始まる文字はbPlacer
     // [a-zA-Z]がないと数字がバリエーション名の中に紛れてしまう
     // 正規表現がややこしくなるため、スペース等はあまり考慮していません
     // .t0-b-xxxxx(数字 or #{$...} or 無)-bPlacer{@if $layout == "N00" {padding-bottom:00;}@else{padding-bottom:99;}}
     // [0]:全体, [1]:クラス名, [2]:t0-, [3]:バリエーション名, [4]:{}の中身（使わない）, [5]:N00がある場合の余白の値, [6]:N00でない余白の値
-    var matched_variation = b_placer_line.match(/(\.(t0-)?b-[\.\_\-a-zA-Z0-9]*[a-zA-Z])(\d*|\#\{\$[a-zA-Z0-9]+\})?-bPlacer{(.+N00.+padding-bottom:(.+?);)?.*padding-bottom:(.+?);}/);
-    if (matched_variation) {
+    const matched_b_placer_line = b_placer_scss_line.match(/(\.(t0-)?b-[\.\_\-a-zA-Z0-9]*[a-zA-Z])(\d*|\#\{\$[a-zA-Z0-9]+\})?-bPlacer{(.+N00.+padding-bottom:(.+?);)?.*padding-bottom:(.+?);}/);
+    const b_placer_line = new BPlacerline(matched_b_placer_line);
+    if (b_placer_line.has_matched()) {
       if (!is_sp) {
-        b_placers.push(create_b_placer(b_placer_base, matched_variation));
+        b_placer_record_list.push(create_b_placer_record(b_placer_base_record, b_placer_line));
       } else {
-        update_sp_value(b_placers, matched_variation);
+        set_sp_value(b_placer_record_list, b_placer_line);
       }
     }
   });
-  return b_placers;
+  return b_placer_record_list;
 }
 
 gulp.task('create-b-placer-doc', (done) => {
@@ -259,8 +290,8 @@ gulp.task('create-b-placer-doc', (done) => {
     return;
   }
 
-  const b_placers = create_b_placers();
-  fs.writeFileSync('devStuff/docs/bPlacer.md', to_md_text(b_placers));
+  const b_placer_record_list = create_b_placer_record_list();
+  fs.writeFileSync('devStuff/docs/bPlacer.md', to_md_text(b_placer_record_list));
   done();
   }
 );
